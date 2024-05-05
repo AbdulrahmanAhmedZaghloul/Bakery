811204d26310e1817c3ee76d1733037c32fa8411
Your Name <you@example.com>
2024-05-05 03:29:58 +0300

    first commit

--- js/index-1.js
+++ js/index-1.js
@@ -1,191 +1,13 @@
-let aboutOffsetTop = $("#down").offset().top;	// topبطول ب ال aboutده بيجيب المسافه م بين اول الموقع و ال offset
-
-$(window).scroll(function () {	// scroll يعمل userانا كده بقول لم ال 
-  let wScroll = $(window).scrollTop();
-
-  if (wScroll > aboutOffsetTop - 50) {
-    $("#main-nav").css("backgroundColor", "rgb(255,255,255)");
-    $("#main-nav").css("border-bottom", "#d4d4d4 solid 1px");
-    $(".navlist a").css("color", "rgb(53, 53, 53)");
-  }
-  else {
-    $("#main-nav").css("backgroundColor", "rgba(0, 0, 0,0.2)");
-    $("#main-nav").css("border-bottom", "none");
-    $(".navlist a").css("color", "rgba(255,255,255,0.8)");
-  }
-})
-
-let arrowOffsetTop = $("#down").offset().top;
-
-$(window).scroll(function () {	// scroll يعمل userانا كده بقول لم ال 
-  let wScroll = $(window).scrollTop();
-
-  if (wScroll > arrowOffsetTop - 50) {	// اظهر السهم about لم انزل ع 
-    $("#btnUp").fadeIn(500);
-  }
-  else {	// لم اطلع اخفي السهم 
-    $("#btnUp").fadeOut(500);
-  }
-})
-$("#btnUp").click(function () {	// لم اضغط ع السهم اطلع ع اول الموقع في 3 ثواني 
-  $("html,body").animate({ scrollTop: 0 }, 100);
-})
-
-let menuIcon = document.querySelector("#menu-icon");
-let navlist = document.querySelector(".navlist");
-menuIcon.onclick = () => {
-  menuIcon.classList.toggle("fa-xmark");
-  navlist.classList.toggle("open");
-};
-
-// $(document).ready(function () {
-//   $("#loed").fadeOut(2000, function () {
-//     $("body").css("overflow", "auto");
-//   });
-// });
-// /////////////////// toggle icon navbar transparent  ////////////////////////
-
-// let x = setTimeout(function () {console.log('hllol')},3000);
-// $(window).scroll(function () {
-//   let doscroll = $(window).scrollTop();
-//   if (doscroll > teoset - 50) {
-//     $("header").css("backgroundColor", "rgba(0, 0, 0)");
-//     $("#butup").fadeIn(500);
-//   } else {
-//     $("header").css("backgroundColor", "rgba(0, 0, 0, 0.4)");
-//     $("#butup").fadeOut(500);
-//   }
-// });
-// /////////////////// ready load fadeout  ////////////////////////
-// ////..../////////////////////////////
-
-////////////////////colorbox///////////////////////////////
-// let boxwidth = $("#colorboxw").outerWidth();
-// $("#side").animate({ left: `-${boxwidth}` }, 0); // يبقي جوه اول م افتح الموقع Boxده علشان ال
-// $("#toggln").click(function () {
-//   if ($("#side").css("left") == "0px") {
-//     $("#side").animate({ left: `-${boxwidth}` }, 1000);
-//   } else {
-//     $("#side").animate({ left: `0px` }, 1000);
-//   }
-// });
-
-// for (let i = 0; i < $(".item").length; i++) {
-//   let red = Math.round(Math.random() * 215);
-//   let red1 = Math.round(Math.random() * 25);
-//   let red2 = Math.round(Math.random() * 25);
-//   $(".item").eq(i).css("backgroundColor", `rgb(${red},${red1},${red2})`);
-// }
-// $(".item").click(function () {
-//   let xcolor = $(this).css("backgroundColor");
-//   $("p ,h1 ,h2,h3,h4,h5,h6,a,span").css("color", xcolor);
-// });
-// ////////////////////colorbox///////////////////////////////
-
-// ////////////////////........///////////////////////////////
-
-// let colorBoxWidth = $("#colorBox").outerWidth(); //colorBoxWidth واحطها في colorBoxانا كده بجيب عرض ال innerWidth
-// $("#sideBar").animate({ right: `-${colorBoxWidth}` }, 0); // يبقي جوه اول م اف  الموقع Boxده علشان ال
-// $("#sideBarToggle").click(function () {
-//     if ($('#sideBar').css("right") == "0px")//البوكس كده بره
-//     {
-//         $("#sideBar").animate({ right: `-${colorBoxWidth}` }, 2000);//right ب السالب ل colorBoxWidthكده لم اضغط علي الاعدادت داخل ال
-//     }
-//     else {
-//         $('#sideBar').animate({ right: `0px` }, 2000);//ده كده يبقي بره	0 = right ب sideBar انا كده بقول لو البوكس
-//     }
-
-// })
-// // Background-img
-
-// $(".box-image img").click(function () {
-//     let imgSrc = $(this).attr("src");
-//     $(".home").css("backgroundImage", `url(${imgSrc})`);
-// });
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
-// document.addEventListener("DOMContentLoaded", function() {
-//   var leadElement = document.querySelector('.lead1');
-//   var count = 0;
-
-//   function updateCounter() {
-//     count++;
-//     leadElement.textContent = count;
-
-//     if (count < 1430) {
-//       setTimeout(updateCounter, 150); // تعديل الفاصل الزمني حسب الحاجة
-//     }
-//   }
-
-//   updateCounter();
-// });
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   var leadElement = document.querySelector('.lead2');
-//   var count = 0;
-
-//   function updateCounter() {
-//     count++;
-//     leadElement.textContent = count;
-
-//     if (count < 64) {
-//       setTimeout(updateCounter, 100); // تعديل الفاصل الزمني حسب الحاجة
-//     }
-//   }
-
-//   updateCounter();
-// });
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   var leadElement = document.querySelector('.lead3');
-//   var count = 0;
-
-//   function updateCounter() {
-//     count++;
-//     leadElement.textContent = count;
-
-//     if (count < 960) {
-//       setTimeout(updateCounter, 200); // تعديل الفاصل الزمني حسب الحاجة
-//     }
-//   }
-
-//   updateCounter();
-// });
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   var leadElement = document.querySelector('.lead4');
-//   var count = 0;
-
-//   function updateCounter() {
-//     count++;
-//     leadElement.textContent = count;
-
-//     if (count < 420) {
-//       setTimeout(updateCounter, 250); // تعديل الفاصل الزمني حسب الحاجة
-//     }
-//   }
-
-//   updateCounter();
-// });
-
-
-
 function toggleClass() {
   var container = document.querySelector('.aswe');
   var dasw = document.querySelector('.dasw');
-
-  // container.classList.toggle('minus');
-  // container.classList.toggle('plus');
-
   if (container.classList.contains('')) {
     dasw.style.display = 'block';
-    toggleIcon.textContent = '^'; // تغيير علامة الإضافة إلى الطرح
+    toggleIcon.textContent = '^'; 
 
   } else {
     dasw.style.display = 'none';
-    toggleIcon.textContent = '^'; // تغيير علامة الطرح إلى الإضافة
+    toggleIcon.textContent = '^'; 
 
   }
 }
@@ -195,11 +17,95 @@ function toggleVisibility() {
 
   if (dasw.style.display === 'none') {
     dasw.style.display = 'block';
-    toggleIcon.textContent = '^'; // تغيير علامة الإضافة إلى الطرح
+    toggleIcon.textContent = '^';
   } else {
     dasw.style.display = 'none';
-    toggleIcon.textContent = '^'; // تغيير علامة الطرح إلى الإضافة
+    toggleIcon.textContent = '^';
   }
 }
+$(document).ready(function () {//jsده اخر حاجه بعملها بعد م اخلاص ال
+
+  let aboutOffsetTop = $("#down").offset().top;	//element ده بيجيب اي مكان  
+
+  $(window).scroll(function () {
+    let wScroll = $(window).scrollTop();	//نزل لحد فين uesrده ال
+
+    if (wScroll > aboutOffsetTop - 50)//aboutال50 ده انا كده بطرح 50 من الطول ال 
+    {
+
+      $("#main-nav").css("backgroundColor", "rgb(255,255,255)");
+      $("#main-nav").css("border-bottom", "#d4d4d4 solid 1px");
+      $(".navlist a").css("color", "rgb(53, 53, 53)");
+      $("#btnUp").fadeIn(500);//انا كده بظهر السهم في نص ثانيه 
+    }
+    else {
+
+      $("#main-nav").css("backgroundColor", "rgba(0, 0, 0,0.2)");
+      $("#main-nav").css("border-bottom", "none");
+      $(".navlist a").css("color", "rgba(255,255,255,0.8)");
+      $("#btnUp").fadeOut(500);//انا كده بخفي السهم في نص ثانيه 
+    }
+  })
+
+  $("#btnUp").click(function () {//ده علشان لم اضغط علي السهم يطلع ل فوق 
+    $("html,body").animate({ scrollTop: 0 }, 1000);//ده كده هيطلع ل فوق 0
+  })
+
+  //------------------
+  //يتحرك بشكل جميل navbarده علشان لم اضغط علي اي لينك في ال
+
+  $("a[href^='#']").click(function () {//ده اللي هو اللينك aال
+    //#اول علامه ال href انا كده بقول لم اضغط علي لينك في  
+
+    let aHref = $(this).attr("href");	//srcهيجيب ال Attributeانا كده بقول هات الattrولم بضغط بيقولي ضغط فين والaبتعود علي لينك ال thisال
+    //setter علشان بعد كده هستخدم aHref وبعد كده حطها في getter ده هستخدم هنا $ (this).attr("href");
 
 
+    //$("#about").offset().top;//aboutانا كده بقول هات ابعاد ال
+    let sectionOffset = $(aHref).offset().top;//كده هيجيب ابعاد اللينكات كلها
+    //setter ده هستخدم هنا aHref 
+
+    $("html,body").animate({ scrollTop: sectionOffset }, 2000);
+  });
+
+  //----------------------------
+  //ده علشان بوكس الالوان
+  let colorBoxWidth = $("#colorBox").outerWidth(); //colorBoxWidth واحطها في colorBoxانا كده بجيب عرض ال innerWidth
+
+  $("#sideBar").css({ left: `-${colorBoxWidth}` }); // يبقي جوه اول م افتح الموقع Boxده علشان ال
+
+  $("#sideBarToggle").click(function () {
+
+
+    if ($('#sideBar').css("left") == "0px")//البوكس كده بره
+    {
+      $("#sideBar").animate({ left: `-${colorBoxWidth}` }, 2000);//left ب السالب ل colorBoxWidthكده لم اضغط علي الاعدادت داخل ال
+    }
+    else {
+      $('#sideBar').animate({ left: `0px` }, 2000);//ده كده يبقي بره	0 = left ب sideBar انا كده بقول لو البوكس
+    }
+
+  })
+
+  //تغير اللون في البوكس 
+  for (let i = 0; i < $(".color-item").length; i++) {
+    let red = Math.round(Math.random() * 255);
+    let green = Math.round(Math.random() * 255);
+    let blue = Math.round(Math.random() * 255);
+
+    $(".color-item").eq(i).css("backgroundColor", `rgb(${red} ,${green} ,${blue} )`);
+  }
+
+  //بيغير بقي الالون اللي انت عايزه boxلم اضغط علي لون من الالون في ال
+  $(".color-item").click(function () {
+    let bkgColor = $(this).css("backgroundColor");
+    $("*").css("color", bkgColor)
+  })
+
+  //-----------------------------
+  // ده علشان لم الموقع يحمل يطلع شكل اعقبال م يحمل 
+  $("#loading").fadeOut(2000, function () {
+    $("body").css("overflow", "auto")
+  });
+
+})
